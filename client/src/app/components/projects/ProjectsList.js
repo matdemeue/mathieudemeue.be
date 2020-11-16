5ec27c2e30829f062942f6d5fd634af6abd75d5b
Mathieu DM <demeuem@gmail.com>
2020-11-16 22:44:12 +0100

    :sparkles: Add general title to projectslist

--- client/src/app/components/projects/ProjectsList.js
+++ client/src/app/components/projects/ProjectsList.js
@@ -1,5 +1,6 @@
 import React from 'react';
 
+import SectionTitle from '../general/SectionTitle';
 import Project from './Project';
 
 import projectData from '../../data/projects.json';
@@ -12,13 +13,16 @@ const ProjectsList = () => {
         <div className="projects_list pb-5">
             <div className="container">
                 <div className="row">
-                        {projectData.map((project, i) => 
-                            <Project key={i} data={project} />
-                        )}
+                    <div className="col-12">
+                        <SectionTitle title={"Recente projecten"} short={"Benieuwd wat ik doe?"} />
+                    </div>
+                    {projectData.map((project, i) => 
+                        <Project key={i} data={project} />
+                    )}
                 </div>
             </div>
         </div>
     )
-}
+} 
 
 export default ProjectsList;
