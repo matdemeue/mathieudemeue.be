9dbfabf6a170e1fccff1666c3709f3fe6cf93b30
Mathieu DM <demeuem@gmail.com>
2020-11-16 22:44:46 +0100

    :sparkles: Add every section to the homepage

--- client/src/app/pages/HomePage.js
+++ client/src/app/pages/HomePage.js
@@ -1,18 +1,23 @@
-
-import React, { } from 'react';
+import React from 'react';
 
 import Hero from '../components/general/Hero';
 import ProjectsList from '../components/projects/ProjectsList';
+import TechnologyList from '../components/technology/TechnologyList';
+import Contact from '../components/contact/Contact';
+import Footer from '../components/general/Footer';
 
 import './HomePage.scss';
 
-const HomePage = ({children}) => {
+const HomePage = () => {
   return (
     <div className="page page__home">
         <div className="bg_dark">
             <div className="container">
                 <Hero />
                 <ProjectsList />
+                <TechnologyList />
+                <Contact />
+                <Footer />
             </div>
         </div>
     </div>
