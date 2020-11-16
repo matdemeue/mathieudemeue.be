4cfd2572fcb9796fb458c8c5416f0f86ec440795
Mathieu DM <demeuem@gmail.com>
2020-11-16 22:50:36 +0100

    :sparkles: Add intro text

--- client/src/app/components/general/Hero.js
+++ client/src/app/components/general/Hero.js
@@ -18,8 +18,9 @@ const Hero = () => {
                     </div>
                     <div className="col-12 col-lg-8">
                         <div class="jumbotron bg_dark mb-0">
-                            <h1 class="display-4 text-light">Hoi! Mathieu is de naam.</h1>
-                            <p class="lead text-light">Cupcake ipsum dolor sit amet soufflé wafer marshmallow. Oat cake muffin wafer. Cake gummi bears lemon drops jujubes tiramisu muffin tootsie roll jelly beans. Cupcake ipsum dolor sit amet soufflé wafer marshmallow. Oat cake muffin wafer. Cake gummi bears lemon drops jujubes tiramisu muffin tootsie roll jelly beans.</p>
+                            <h1 class="display-4 text-light mb-4">It's-a Me, Mario... <br></br> Ugh I mean Mathieu.</h1>
+                            <p class="lead text-light">Hello there! <br></br> Ik ben Mathieu De Meue. Als ik een goede persoonlijke omschrijving moet geven kan ik dit verkorten tot volgende drie woorden: leergierig, teamspeler en oplossingsgericht. <br></br> <br></br >Werk gerelateerd hecht ik veel belang aan de netheid en precisie van mijn code. Bovendien ben ik zeer gedisciplineerd en kan ik goed omgaan met strikte deadlines. 
+                            </p>
                             <a class="btn btn-light btn-lg mt-4" href={curriculum_vitae} role="button">Download mijn CV</a>
                         </div>
                     </div>
