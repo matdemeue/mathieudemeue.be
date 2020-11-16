4917aab6fe37d9b4ffa0f905d707f5a5195c2aa2
Mathieu DM <demeuem@gmail.com>
2020-11-16 14:08:26 +0100

    :sparkles: Add curriculum vitae

--- client/src/app/components/general/Hero.js
+++ client/src/app/components/general/Hero.js
@@ -1,6 +1,7 @@
 import React from 'react';
 
 import profielfoto from '../../images/general/mathieu.jpg';
+import curriculum_vitae from '../../downloads/cv_mathieu.pdf';
 
 import './Hero.scss';
 
@@ -19,7 +20,7 @@ const Hero = () => {
                         <div class="jumbotron bg_dark mb-0">
                             <h1 class="display-4 text-light">Hoi! Mathieu is de naam.</h1>
                             <p class="lead text-light">Cupcake ipsum dolor sit amet soufflé wafer marshmallow. Oat cake muffin wafer. Cake gummi bears lemon drops jujubes tiramisu muffin tootsie roll jelly beans. Cupcake ipsum dolor sit amet soufflé wafer marshmallow. Oat cake muffin wafer. Cake gummi bears lemon drops jujubes tiramisu muffin tootsie roll jelly beans.</p>
-                            <a class="btn btn-light btn-lg mt-4" href="#" role="button">Download mijn CV</a>
+                            <a class="btn btn-light btn-lg mt-4" href={curriculum_vitae} role="button">Download mijn CV</a>
                         </div>
                     </div>
                 </div>
